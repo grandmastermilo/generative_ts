59ba72bad23403f48a074e3b35bc1cbd517e0100
grandmastermilo <miloc@btinternet.com>
2023-01-21 15:32:25 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A repository for eploring different methods for generating timeseries data for the case of limited training data',
+    author='Milo Carroll',
+    license='MIT',
+)
